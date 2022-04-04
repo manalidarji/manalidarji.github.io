fb4a3321bf1de70ab750bb4a771d25376881aec9
Manali Darji <darjimanali93@gmail.com>
2022-04-04 16:54:46 -0400

    add typing effect for hero title section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,22 @@
-let mobMenu = document.getElementById('mob-header-menu');
+async function typeSentence(sentence, eleRef, delay = 100) {
+    const letters = sentence.split('');
+    let i = 0;
+    while(i < letters.length) {
+      await waitForMs(delay);
+      document.getElementById(eleRef).innerHTML += letters[i];
+      i++;
+    }
+    return;
+}    
+function waitForMs(ms) {
+return new Promise(resolve => setTimeout(resolve, ms))
+}
 
+// typing effect for hero section
+typeSentence('I am Manali Darji.', 'about-title', 180);
+
+
+let mobMenu = document.getElementById('mob-header-menu');
 document.getElementById('hamburger').addEventListener('click', function(e){
     e.preventDefault;
     mobMenu.classList.add('active');
@@ -9,4 +26,3 @@ document.getElementById('close-mob-menu').addEventListener('click', function(e){
     e.preventDefault;
     mobMenu.classList.remove('active');
 })
-
