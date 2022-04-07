301abd01e8a4f8299c69ab4450ebdd8348b8a820
Manali Darji <darjimanali93@gmail.com>
2022-04-07 14:30:06 -0400

    add portfolio section

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,78 @@ document.getElementById('close-mob-menu').addEventListener('click', function(e){
     e.preventDefault;
     mobMenu.classList.remove('active');
 })
+
+// portfolio section
+const projects = [
+    {
+        title: 'My Portfolio Site',
+        image: 'proj_1.png',
+        desc: 'My personal website project which tells you about me, skills, experience, education, my projects and a way to contact me.',
+        stack: 'HTML, CSS, JS',
+        gitLink: 'https://github.com/manalidarji/manalidarji.github.io'
+    },
+    {
+        title: 'Relax-Inn',
+        image: 'proj_2.png',
+        desc: 'Relax-Inn, a fictional company, allows visitors to find and/or book vacation rentals, cabins, beach houses and unique homes, around the world, specifically for short-term rental.',
+        stack: 'HTML, CSS, Javascript, React, JSON-Server',
+        gitLink: 'https://github.com/manalidarji/relax-inn'
+    },
+    {
+        title: 'Who wants to be Millionaire?',
+        image: 'proj_3.png',
+        desc: "This project is a JAVA Console App, programmed using core JAVA & Object-Oriented Programming(OOP), inspired from famous Quiz game: 'Who wants to be Millionaire'",
+        stack: 'Core JAVA',
+        gitLink: 'https://github.com/manalidarji/JAVA-Quiz-Game'
+    },
+    {
+        title: 'Betting Blog Site',
+        image: 'proj_4.png',
+        desc: "Betting blog site is a blog-publishing system using custom theme on WordPress, which is a content management system(CMS).",
+        stack: 'HTML, CSS, Javascript, PHP, Wordpress, MySQL',
+        gitLink: 'https://github.com/manalidarji/betting-blog-site'
+    }
+];
+
+const allProjectsNode = document.getElementById('all-projects-wrap');
+projects.forEach(project => {
+    const projectNode = `
+    <div class="project-wrap"> 
+        <a class="project-a" target="_blank" href="${project.gitLink}">   
+            <div class="project-img-wrap">
+                <img class="img-resp" src="./assets/images/${project.image}" alt="Project"/>
+            </div>
+            <p class="proj-title-display">${project.title}</p>
+            <div class="project-content-overlay">
+                <div class="project-content">
+                    <h6 class="project-title">${project.title}</h6>
+                    <p class="project-desc">${project.desc}</p>
+                    <p class="project-stack">Tech Stack: ${project.stack}</p>
+                    <button class="btn">View Source Code</button>
+                </div>
+            </div>
+        </a>
+    </div>
+    `;
+    allProjectsNode.innerHTML += projectNode;
+})
+
+
+
+
+
+
+// function scrollTo(element, to, duration) {
+//     if (duration <= 0) return;
+//     var difference = to - element.scrollTop;
+//     var perTick = difference / duration * 10;
+
+//     setTimeout(function() {
+//         element.scrollTop = element.scrollTop + perTick;
+//         if (element.scrollTop === to) return;
+//         scrollTo(element, to, duration - 10);
+//     }, 10);
+// }
+
+// elmnt = document.getElementById('experience');
+// scrollTo(document.body, elmnt.offsetTop, 600);
